0ae0a432c6789b14d5ba387de54ba94b6567dc11
Shira Gilliam <shirahagar.gilliam@post.runi.ac.il>
2024-01-12 12:26:16 +0200

    HW4

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,79 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int n = array.length;
+        int sumFullArr = 0;
+        int sumMissingArr = 0;
+        int missing = -1;
+        for(int i = 0; i <= n; i++) {
+            sumFullArr = sumFullArr + i;
+        }
+        for(int j = 0; j < n; j++) {
+            sumMissingArr = sumMissingArr + array[j];
+        }
+        missing = sumFullArr - sumMissingArr;
+        return missing;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+       int n = array.length;
+       int maxValue = 0;
+       int secondMax = 0;
+       for(int i = 0; i < n; i++) {
+        if(maxValue < array[i]) {
+            secondMax = maxValue;
+            maxValue = array[i];
+        } else if (secondMax < array[i]) {
+            secondMax = array[i];
+        }
+        }
+        return secondMax;
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+    public static boolean containsTheSameElements(int [] array1, int [] array2) {
+        int n1 = array1.length;
+        int n2 = array2.length;
+        for(int i = 0; i < n1; i++) {
+			int count = 0;
+            for(int j = 0; j < n2; j++) {
+                if(array1[i] == array2[j]) {
+                    count++;
+                }
+            } 
+            if(count == 0) {
+                return false;
+            }
+        }
+        for(int i = 0; i < n2; i++) {
+			int count = 0;
+            for(int j = 0; j < n1; j++) {
+                if(array1[j] == array2[i]) {
+                    count++;
+                }
+            } 
+            if(count == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        int n = array.length;
+        boolean increase = true;
+        boolean decrease = true;
+       for(int i = 0; i < (n - 1); i++) {
+        if (array[i] < array[i + 1]) {
+            decrease = false;
+        } else if(array[i] > array[i + 1]) {
+            increase = false;
+        } 
     }
-
+    if(decrease != increase) {
+    return true;
+    }
+    return false;
+}
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,101 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
+        String newSentence = "";
+        String lastSentence = "";
+        String vowels = "aeiou";
+		for(int i = 0; i < string.length(); i++) {
+			char currentChar = string.charAt(i);
+			if(currentChar > 'A' && currentChar < 'Z') {
+                currentChar = (char)(currentChar + 32);
+            }
+            newSentence = newSentence + currentChar;
+        }
+        for(int i = 0; i < newSentence.length(); i++) {
+			char currentChar = newSentence.charAt(i);
+            if(vowels.indexOf(currentChar) != -1) {
+                currentChar = (char)(currentChar - 32);
+            }
+			lastSentence = lastSentence + currentChar;
+        }
+        return lastSentence;
+        }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase(String string) {
+        int indexSpace = 0;
+        String fullSentence = "";
+        char lastLetter = string.charAt(string.length() - 1);
+            
+        while (string.charAt(indexSpace) == ' ') {
+            indexSpace++;
+        }
+    
+        boolean notSpace = true;
+        boolean newWord = false;
+        for (int i = indexSpace; i < (string.length() - 1); i++) {
+            char currentChar = string.charAt(i);
+            char firstLetter = string.charAt(i + 1);
+    
+            if (currentChar == ' ') {
+                notSpace = false;
+            } else {
+                notSpace = true;
+            }
+    
+            if ((currentChar >= 'A' && currentChar <= 'Z') && notSpace) {
+                    currentChar = (char)(currentChar + 32);
+                }
+    
+            if (currentChar == ' ' && firstLetter != ' ') {
+                newWord = true;
+            }
+    
+            if (currentChar != ' ' && newWord) {
+                newWord = false;
+                if (currentChar <= 'z' && currentChar >= 'a' && notSpace) {
+                    currentChar = (char)(currentChar - 32);
+                }
+            }
+                
+            if (currentChar != ' ') {
+                fullSentence = fullSentence + currentChar;
+            }
+        }
+    
+        if (lastLetter >= 'a' && lastLetter <= 'z') {
+            fullSentence = fullSentence + lastLetter;
+        } else if (lastLetter >= 'A' && lastLetter <= 'Z') {
+            lastLetter = (char)(lastLetter + 32);
+            fullSentence = fullSentence + lastLetter;
+        } else if (lastLetter >= 'A' && lastLetter <= 'Z' && string.charAt(string.length() - 2) == ' ') {
+            fullSentence = fullSentence + lastLetter;
+        }
+    
+        return fullSentence;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int size = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char currentChar = string.charAt(i);
+            if(currentChar == chr) {
+                size++;    
+            }
+        }
+
+        int[] index = new int[size];
+        int j = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char currentChar = string.charAt(i);
+            if(currentChar == chr) {   
+                index[j] = i;
+                j++;
+            }
+        }
+
+        return index;
     }
 }
